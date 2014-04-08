c327c02d9f5581f029e73045e595a762183aafa8
Kai Davenport <kaiyadavenport@gmail.com>
2014-04-08 11:14:01 +0000

    date constructor looks after being passed a date

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 var resolutions = require('time-resolutions');
 
-function get_date(d){
-	if(typeof(d)==="number"){
-		d = new Date(d);
-	}
-	return d;
-}
-
 module.exports = function timestamp_range(start, end){
 	if(arguments.length<=0){
 		throw new Error('start and/or end needed for timestamp range');
 	}
 
-	start = get_date(start);
-	end = get_date(end || new Date());
-
-	var starttime = start.getTime();
-	var endtime = end.getTime();
+	if(!end){
+		end = new Date();
+	}
+	
+	var starttime = new Date(start).getTime();
+	var endtime = new Date(end).getTime();
 
 	if(endtime<starttime){
 		throw new Error('end time cannot be before start time')
